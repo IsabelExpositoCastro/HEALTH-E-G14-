11e211a23686083cd792b0fa64a2bb361291fe15
iAau199 <itxaro.aizpurua01@estudiant.upf.edu>
2024-06-04 12:46:20 +0200

    last changes

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -2,7 +2,11 @@ import streamlit as st
 telephone_number = "(34) 123 123 123"
 def display():
     st.header("How to contact us?")
-    st.write("Email :   support@healthe.com")
-    st.write("Phone number:    ")
-    st.markdown(f'<a href="tel:+1234567890">{telephone_number}</a>', unsafe_allow_html=True)
+    st.write("We would love to hear from you! At HEALTH-E, we are here to help you and answer all your questions.")
+    st.write("If you have any queries, comments or need assistance with our application, please do not hesitate to contact us via the options below:")
+    lst = [' E-mail: info@healthe.com', 'Phone: +0034 900 100 200']
+    for i in lst:
+        st.markdown("- " + i)
+    st.write("Customer service hours: Monday to Friday, 9:00 AM - 6:00 PM (Local Time)")
+    st.write("At HEALTH-E, our goal is to provide you with the best possible experience. If you have any suggestions or comments on how we can improve, we'd love to hear from you. Thank you for trusting us with your healthcare needs.")
     
--- pages/profile.py
+++ pages/profile.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from pages import privacy_security, contact_us
 
 def display(user):
@@ -30,10 +31,26 @@ def display(user):
                 privacy_security.display()
                 # Additional code for Privacy & Security goes here
             elif option == "My Saved Prescriptions":
-                st.write("This is where you can view your saved prescriptions.")
+                st.subheader("My Saved Prescriptions")
+                # Example prescription data
+                prescription_data = {
+                    "PRESCRIPTION": ["Paracetamol 1g", "Frenadol Forte (sobres)", "Ibuprofeno 600mg"],
+                    "STARTING DATE": ["22/05/2022", "12/03/2023", "12/03/2023"],
+                    "ENDING DATE": ["25/05/2022", "17/03/2023", "17/03/2023"]
+                }
+                
+                df = pd.DataFrame(prescription_data)
+                st.table(df)
                 # Additional code for My Saved Prescriptions goes here
             elif option == "See My Appointments":
-                st.write("This is where you can view your appointments.")
+                st.subheader("My Appointments")
+                cols = st.columns([1, 2, 1])
+                with cols[1]:
+                    if st.button("Future Appointments"):
+                        st.write("Displaying future appointments...")
+
+                    if st.button("Past Appointments"):
+                        st.write("Displaying past appointments...")
                 # Additional code for See My Appointments goes here
             elif option == "Contact Us":
                 contact_us.display()
